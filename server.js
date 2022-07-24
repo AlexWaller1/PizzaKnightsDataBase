7c48f753009a2d850063d36cffda2e5702016a3a
Alex Waller <alex@artofalexwaller.com>
2022-07-24 18:06:47 -0400

    setting up port

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const port = 3012;
+
+const app = express();
+
+app.listen(port, () => console.log(`Server Started on port ${port}`));
