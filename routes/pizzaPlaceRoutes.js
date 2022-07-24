3c0b3902a25dbf7e2e261a2125a745f2a2c19e2a
Alex Waller <alex@artofalexwaller.com>
2022-07-24 19:34:11 -0400

    setting up Pizza Place Routes

--- routes/pizzaPlaceRoutes.js
+++ routes/pizzaPlaceRoutes.js
@@ -5,4 +5,16 @@ router.get("/", (req, res) => {
   res.send({ message: "Get Pizza Places" });
 });
 
+router.post("/", (req, res) => {
+  res.send({ message: "Post Pizza Places" });
+});
+
+router.put("/:id", (req, res) => {
+  res.send({ message: `Update Pizza Place ${req.params.id}` });
+});
+
+router.delete("/:id", (req, res) => {
+  res.send({ message: `Delete Pizza Place ${req.params.id}` });
+});
+
 module.exports = router;
