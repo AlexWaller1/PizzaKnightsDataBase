19f44407c47b5fc4f8d4118bfee65b1d1a944045
Alex Waller <alex@artofalexwaller.com>
2022-07-24 19:16:16 -0400

    routes

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const port = process.env.PORT || 3012;
 
 const app = express();
 
+app.get("/api/pizzaPlaces", (req, res) => {
+  res.send({ message: "Get Pizza Places" });
+});
+
 app.listen(port, () => console.log(`Server Started on port ${port}`));
