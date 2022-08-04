c51bb7fd3bfed84b7306a06cee43c51078bbf20e
Alex Waller <alex@artofalexwaller.com>
2022-08-03 23:54:47 -0400

    setting up database

--- controllers/pizzaPlacesController.js
+++ controllers/pizzaPlacesController.js
@@ -1,34 +1,35 @@
+const asyncHandler = require("express-async-handler");
 // @description Get Pizza Places
 // @route GET /api/pizzaPlaces
 // @access Private
-const getPizzaPlaces = async (req, res) => {
+const getPizzaPlaces = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get Pizza Places" });
-};
+});
 
 // @description Post Pizza Places
 // @route POST /api/pizzaPlaces
 // @access Private
-const postPizzaPlace = async (req, res) => {
+const postPizzaPlace = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text field");
   }
   res.status(200).json({ message: "Post Pizza Place" });
-};
+});
 
 // @description Update Pizza Places
 // @route PUT /api/pizzaPlaces/:id
 // @access Private
-const updatePizzaPlace = async (req, res) => {
+const updatePizzaPlace = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update Pizza Place ${req.params.id}` });
-};
+});
 
 // @description Delete Pizza Places
 // @route DELETE /api/pizzaPlaces/:id
 // @access Private
-const deletePizzaPlace = async (req, res) => {
+const deletePizzaPlace = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete Pizza Place ${req.params.id}` });
-};
+});
 
 module.exports = {
   getPizzaPlaces,
